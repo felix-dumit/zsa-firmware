a992eed3852c02a267488de17dbe9f7defb0251c
Felix Dumit <felix.dumit@gmail.com>
2024-03-15 23:23:13 -0600

    edit again

--- mylayout/rules.mk
+++ mylayout/rules.mk
@@ -1,6 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-# manual edit
+# manual edit 2
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 ORYX_ENABLE = yes
